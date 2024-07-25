ffd0e880696ee36d795adefa4e9732696b480d5f
Rodrigo Chacon <321351+rochacon@users.noreply.github.com>
2024-07-25 20:11:30 -0300

    ensure dockerfile and go tree sitter

--- init.lua
+++ init.lua
@@ -1077,7 +1077,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'dockerfile', 'go', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
