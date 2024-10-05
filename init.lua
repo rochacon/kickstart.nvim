255b5f71222c7b0126e4c5a71709878a3c41fc42
Rodrigo Chacon <321351+rochacon@users.noreply.github.com>
2024-10-05 15:45:02 -0300

    init.lua: replace tsserver w/ ts_ls

--- init.lua
+++ init.lua
@@ -756,7 +756,7 @@ require('lazy').setup({
         ruff_lsp = {}, -- Python
         rust_analyzer = {},
         terraformls = {},
-        tsserver = {},
+        ts_ls = {},
         yamlls = {},
         vimls = {},
         zls = {}, -- Zig
@@ -812,11 +812,6 @@ require('lazy').setup({
             local nvim_lsp = require 'lspconfig'
             nvim_lsp.denols.setup {
               root_dir = nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc'),
-            }
-
-            nvim_lsp.tsserver.setup {
-              root_dir = nvim_lsp.util.root_pattern 'package.json',
-              single_file_support = false,
             }
           end,
         },
