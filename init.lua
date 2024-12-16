9778ef67cbe3ea58c625a70f039ead212426abe8
Rodrigo Chacon <321351+rochacon@users.noreply.github.com>
2024-12-15 22:57:01 -0300

    lsp setup tsserver
    Signed-off-by: Rodrigo Chacon <321351+rochacon@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -773,7 +773,7 @@ require('lazy').setup({
         ruff = {}, -- Python
         rust_analyzer = {},
         terraformls = {},
-        ts_ls = {},
+        tsserver = {},
         yamlls = {},
         vimls = {},
         zls = {}, -- Zig
@@ -829,6 +829,10 @@ require('lazy').setup({
             local nvim_lsp = require 'lspconfig'
             nvim_lsp.denols.setup {
               root_dir = nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc'),
+            }
+            nvim_lsp.tsserver.setup {
+              root_dir = nvim_lsp.util.root_pattern 'package.json',
+              single_file_support = false,
             }
           end,
         },
