cc995a5bd29bf912d2fbd3383c6e13e5d028f1af
Rodrigo Chacon <321351+rochacon@users.noreply.github.com>
2025-01-03 23:19:51 -0300

    configure harpoon on customs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,10 +3,30 @@
 --
 -- See the kickstart.nvim README for more information
 
--- 
--- vim.cmd.colorscheme 'rochacon'
+return {
+  {
+    'theprimeagen/harpoon',
+    config = function()
+      vim.keymap.set('n', 'ha', function()
+        require('harpoon.mark').add_file()
+      end, { desc = '[h]arpoon [a]dd file' })
 
--- You can configure highlights by doing something like:
--- vim.cmd.hi 'Comment gui=none'
+      vim.keymap.set('n', 'hh', require('harpoon.ui').toggle_quick_menu, { desc = '[h]arpoon quick [h]' })
+      vim.keymap.set('n', 'hn', require('harpoon.ui').nav_next, { desc = '[h]arpoon next [n]' })
+      vim.keymap.set('n', 'h,', require('harpoon.ui').nav_prev, { desc = '[h]arpoon prev [,]' })
 
-return {}
+      -- not really related, but inspired and for testing only
+      vim.keymap.set('n', '<leader>h.', ':!tmux split-window -d air<CR>', { desc = 'tmux split: air [h][.]' })
+      vim.keymap.set(
+        'n',
+        '<leader>ht',
+        ':!tmux split-window -h bash -c "go test ./... ; echo press ENTER to quit; read"<CR>',
+        { desc = 'tmux split: go test [h][t]' }
+      )
+
+      -- rochacon colorscheme
+      vim.cmd.colorscheme 'rochacon'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+}
