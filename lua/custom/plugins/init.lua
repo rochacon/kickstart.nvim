6c016dd13496f62ef4f09cc6cbf6fe56bf5da644
Rodrigo Chacon <321351+rochacon@users.noreply.github.com>
2025-03-22 15:15:09 -0300

    workflow updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,13 +7,13 @@ return {
   {
     'theprimeagen/harpoon',
     config = function()
-      vim.keymap.set('n', 'ha', function()
+      vim.keymap.set('n', '<leader>ha', function()
         require('harpoon.mark').add_file()
       end, { desc = '[h]arpoon [a]dd file' })
 
-      vim.keymap.set('n', 'hh', require('harpoon.ui').toggle_quick_menu, { desc = '[h]arpoon quick [h]' })
-      vim.keymap.set('n', 'hn', require('harpoon.ui').nav_next, { desc = '[h]arpoon next [n]' })
-      vim.keymap.set('n', 'h,', require('harpoon.ui').nav_prev, { desc = '[h]arpoon prev [,]' })
+      vim.keymap.set('n', '<leader>hh', require('harpoon.ui').toggle_quick_menu, { desc = '[h]arpoon quick [h]' })
+      vim.keymap.set('n', '<leader>hn', require('harpoon.ui').nav_next, { desc = '[h]arpoon next [n]' })
+      vim.keymap.set('n', '<leader>h,', require('harpoon.ui').nav_prev, { desc = '[h]arpoon prev [,]' })
 
       -- not really related, but inspired and for testing only
       vim.keymap.set('n', '<leader>h.', ':!tmux split-window -d air<CR>', { desc = 'tmux split: air [h][.]' })
