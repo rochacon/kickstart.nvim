41a22355d2cc8d4dcff72aa75f319710aef49f54
Rodrigo Chacon <321351+rochacon@users.noreply.github.com>
2025-07-04 00:29:44 -0300

    set blink keymap to super-tab

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -26,7 +26,6 @@ return {
   },
 
   -- 'saadparwaiz1/cmp_luasnip',
-
   -- Adds other completion capabilities.
   --  nvim-cmp does not ship with all sources by default. They are split
   --  into multiple repos for maintenance purposes.
