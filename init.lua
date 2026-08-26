6c3e1fc1f9a6bbc2febb8da05a1b1160a3f98eaa
Rodrigo Chacon <321351+rochacon@users.noreply.github.com>
2026-08-26 15:48:58 +0000

    fix nvim-treesitter config rename

--- init.lua
+++ init.lua
@@ -84,6 +84,7 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -936,7 +937,7 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      require('nvim-treesitter.configs').setup {
+      require('nvim-treesitter.config').setup {
         highlight = { enable = true },
         indent = { enable = true },
       }
